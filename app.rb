6086cd7e0e69412becb1869eb43c647085c433e1
szakon <sarahzakon@gmail.com>
2018-07-17 19:39:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	
+	get '/hello' do 
+	  erb :Hello
+	end
+	
+	get '/goodbye' do 
+	  erb :goodbye
+	end
+	
+	get '/date' do 
+	  erb :date
 	end
 
 
